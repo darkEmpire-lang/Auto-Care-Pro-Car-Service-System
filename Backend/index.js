2531426502b91720901dac8a1b72bf5f5bb832b7
pasindu maduranga <it22546784@my.sliit.lk>
2024-07-13 22:34:00 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
 const path = require('path');
-const app = express();
 
-// Allow requests from specific origins
+const app = express();
+const PORT = process.env.PORT || 8005;
+const MONGODB_URI = process.env.MONGODB_URL;
+
+
+// Middleware
 const corsOptions = {
     origin: 'https://aute-care-pro-car-service.netlify.app',
     credentials: true,  // enable set cookie
@@ -14,20 +20,28 @@ app.use(express.json());
 
 // Serve static files from the Vite app
 app.use(express.static(path.join(__dirname, 'client', 'build')));
+// Routes
+app.use('/api/auth', require('./routes/auth'));
 
-// Example route
-app.post('/api/auth/login', (req, res) => {
-    // Handle login logic
-    // Example response
-    res.status(200).json({ message: 'Login successful' });
-});
+// Connect to MongoDB
+mongoose.connect(MONGODB_URI, {
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.error('MongoDB connection error:', err));
+
+
+app.get("/",(req,res)=>{
+
+    res.json("Hello")
+})
 
 // Serve index.html for any other routes
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 8005;
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
