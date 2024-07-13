f8ac35e9ed0f663d001a20e7bbacd4cea71f75bd
pasindu maduranga <it22546784@my.sliit.lk>
2024-07-13 20:48:01 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const MONGODB_URI = process.env.MONGODB_URL;
 
 // Middleware
 app.use(cors({
-    origin: 'https://aute-care-pro-car-service.netlify.app/',
+    origin: 'https://aute-care-pro-car-service.netlify.app',
     credentials: true,
 }));
 
@@ -25,20 +25,23 @@ app.use('/api/auth', require('./routes/auth'));
 // Connect to MongoDB
 mongoose.connect(MONGODB_URI, {
     useUnifiedTopology: true,
+    useNewUrlParser: true,
 })
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.error('MongoDB connection error:', err));
 
-app.get("/",(req,res)=>{
+// Basic route to check if server is running
+app.get("/", (req, res) => {
+    res.json("Hello");
+});
 
-    res.json("Hello")
-})
 // Serve React frontend for any other routes
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
