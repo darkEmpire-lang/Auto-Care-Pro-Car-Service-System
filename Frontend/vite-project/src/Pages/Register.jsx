4c33ab4392dbd807639bddb34220c05563c765bb
pasindu maduranga <it22546784@my.sliit.lk>
2024-07-14 11:12:59 +0530

    Update Register.jsx

--- Frontend/vite-project/src/Pages/Register.jsx
+++ Frontend/vite-project/src/Pages/Register.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
 
-const API_URL = 'https://your-backend-url.vercel.app'; // Replace with your actual backend URL
+const API_URL = 'https://auto-care-pro-car-service-system.vercel.app/'; // Replace with your actual backend URL
 
 const Register = () => {
     const [user, setUser] = useState({
